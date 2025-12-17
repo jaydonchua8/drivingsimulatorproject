8d4b59cd92dae1f69e329c6ed9ef9fcc0416ebab
Mohith Raj Sendil Kumar <mohithsendilkumar@gmail.com>
2025-12-16 23:10:32 -0600

    Add new image for wheel motor coupling
    Added a new image for the wheel motor coupling.

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+<img
+  src="/timeline/Photo 1.jpeg"
+  alt="Final setup wide shot"
+  style={{ width: "100%", borderRadius: "16px" }}
+/>
+
+{/* NEW IMAGE */}
+<img
+  src="/timeline/Photo 2.jpeg"
+  alt="Wheel motor coupling close-up"
+  style={{ width: "100%", borderRadius: "16px", marginTop: "12px" }}
+/>
